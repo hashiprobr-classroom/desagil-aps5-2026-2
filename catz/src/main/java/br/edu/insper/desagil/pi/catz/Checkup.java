332bc2eb781efd2c91b76ddf96073074274b9140
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2026-09-23 10:52:09 -0300

    Cria superclasse Procedimento para Banho, Tosa e Checkup

--- catz/src/main/java/br/edu/insper/desagil/pi/catz/Checkup.java
+++ catz/src/main/java/br/edu/insper/desagil/pi/catz/Checkup.java
@@ -1,19 +1,7 @@
 package br.edu.insper.desagil.pi.catz;
 
-public class Checkup {
-    private Cliente cliente;
-    private String descricao;
-
+public class Checkup extends Procedimento {
     public Checkup(Cliente cliente, String descricao) {
-        this.cliente = cliente;
-        this.descricao = descricao;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public String getDescricao() {
-        return descricao;
+        super(cliente, descricao);
     }
 }
--- catz/src/main/java/br/edu/insper/desagil/pi/catz/Tosa.java
+++ catz/src/main/java/br/edu/insper/desagil/pi/catz/Tosa.java
@@ -1,19 +1,7 @@
 package br.edu.insper.desagil.pi.catz;
 
-public class Tosa {
-    private Cliente cliente;
-    private String descricao;
-
+public class Tosa extends Procedimento {
     public Tosa(Cliente cliente, String descricao) {
-        this.cliente = cliente;
-        this.descricao = descricao;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
+        super(cliente, descricao);
     }
 }
